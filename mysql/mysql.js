ab82596043973c027fa0573162d942a5ffa111a3
jian <1475186481@qq.com>
2022-07-14 23:31:33 +0800

    1

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -12,11 +12,11 @@ const {
 const { queryMyspl } =require('../utils/operationMysql')
 async function mysqlConnection (params) {
     const {querySql,res,isSearchList, checkParams, isCheckSso = true} =params;
-    // const isProd = res.req.headers.host.includes('114.215.183.5')
+    const isProd = !process.argv.includes('--dev')
     // 和本地数库建立连接
     var connection = mysql.createConnection({
-      connectionLimit: 50,
-      host: 1 ? 'localhost' : '114.215.183.5', //远程MySQL数据库的ip地址
+      connectionLimit: 500,
+      host: isProd ? 'localhost' : '114.215.183.5', //远程MySQL数据库的ip地址
       user: "build_platform",
       password: "AhAtcEyyjSeSijWE",
       database: "build_platform"
@@ -60,7 +60,24 @@ async function mysqlConnection (params) {
         },[])
         return Promise.all(promiseAll)
     }
-    return new Promise((resolve,reject)=>{
+    return new Promise((res_,rej_)=>{
+            const connectionEnd = () =>{
+                //停止链接数据库，必须再查询语句后，要不然一调用这个方法，就直接停止链接，数据操作就会失败
+                connection.end(function(err){
+                    if(err){
+                        console.log('关闭数据库连接失败！');
+                        throw err;
+                    }
+                });
+            }
+            const resolve = (data) =>{
+                res_(data)
+                connectionEnd()
+            }
+            const reject = (data) =>{
+                rej_(data)
+                connectionEnd()
+            }
             connection.query(querySql,(err,result)=>{
                 if(err){
                     res.send(success(false, {msg: `SQL error: ${err}!`}));
