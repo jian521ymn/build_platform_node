24c49afb91576137353d89b149ce5a8b05eda073
wb_jian <wb_jian@meituan.com>
2022-07-29 19:44:57 +0800

    1

--- routes/api/product/index.js
+++ routes/api/product/index.js
@@ -81,13 +81,14 @@ route.post('/add', (req, res) => {
 //=> 项目删除
 route.get('/delete', (req, res) => {
     const {
-        item_key
+        id
     } = req.query;
     const loginQuerySql = updateMyspl({
         name: "PRODUCT",
         primaryKey: {
-            key: 'item_key',
-            value: item_key
+            key: 'id',
+            value: id,
+            isString:false,
         },
         params: {
             isDelete: "1",
@@ -108,13 +109,13 @@ route.get('/delete', (req, res) => {
 //=> 项目详情
 route.get('/details', (req, res) => {
     const {
-        item_key
+        id
     } = req.query;
     const loginQuerySql = queryMyspl({
         name: "PRODUCT",
         params: {
             isDelete: "0",
-            item_key
+            id
         }
     })
     mysqlConnection({
@@ -132,31 +133,23 @@ route.get('/details', (req, res) => {
 //=> 项目更新
 route.post('/edit', (req, res) => {
     const {
-        item_key,
-        level,
-        name,
-        origin_ssh_url,
-        release_num,
-        type,
-        remark_name
+        id,
+        product_name,
+        product_desc,
+        product_url,
     } = req.body;
     const { userName='' } = req.query
     const loginQuerySql = updateMyspl({
         name: "PRODUCT",
         primaryKey: {
-            key: 'item_key',
-            value: item_key,
+            key: 'id',
+            value: id,
         },
         params: {
             isDelete: "0",
-            level,
-            name,
-            origin_ssh_url,
-            release_num,
-            type,
-            remark_name,
-            operating_time: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-            operator: userName
+            product_name,
+            product_desc,
+            product_url,
         }
     })
     mysqlConnection({
@@ -172,91 +165,23 @@ route.post('/edit', (req, res) => {
         })
 });
 
-//=> 分支拉取更新
-route.get('/branch', (req, res) => {
-    const {name} =req.query
-    if(!name){
-        res.send(success(false,{msg:"仓库名必填！"}))
-    }
-    // 先更新分支，再获取所有分支
-    execPromise(`cd /www/code/${name}  && git branch -r`).then(res_ => {
-        const {err,stdout} =res_ || {};
-        if(err){
-            res.send(success(false,{msg:err?.err}))
-            return
-        }
-        let allBranch = []
-        // 处理git branch 的结果，
-        stdout.split('\n').forEach(item => {
-            const branch = item.replace(/(\s)/g, '').replace(/\*/g, '');
-            // 忽略为空的分支和远程分支
-            if (branch && !branch.includes('HEAD')) {
-                allBranch.push({
-                    value:branch.split('/')[1],
-                    label:branch.split('/')[1]
-                })
-            }
-        })
-        // 得到分支列表
-        res.send(success(true, {
-            data: allBranch
-        }));
-    });
-});
 
-//更新发布状态
-const updateStaus=(item_key,params,res,req)=>{
-    const { userName } = req.query
-    const loginQuerySql = updateMyspl({
-        name: "PRODUCT",
-        primaryKey: {
-            key: 'item_key',
-            value: item_key,
-        },
-        params: {
-            isDelete: "0",
-            ...params,
-            operating_time: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-            operator: userName
-        }
-    })
-   return mysqlConnection({
-            res,
-            querySql: loginQuerySql,
-    })
-}
-// 创建或更新部署记录
-const createOrUpdateStaus=(createId,params,res,req)=>{
-    const { userName='' } = req.query
-    const {page,status} = params;
-    let page_ = ''
-    if(status === 1 && page){
-        page_ = page
-        delete params.page
-    }
-    const createSql = addMyspl({
-        name:"BUILD_INFO_RECORD",
-        params:{...params },
-        page
-    })
-    const loginQuerySql = updateMyspl({
-        name: "BUILD_INFO_RECORD",
-        primaryKey: {
-            key: 'id',
-            value: createId,
-        },
+// 创建部署记录
+route.post('/record_create', (req, res) => {
+    const params =req.body
+    const querySql = addMyspl({
+        name: "PRODUCT_RECORD",
         params: {
             ...params,
             operating_time: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-            operator: userName
         }
     })
-    const querySql = status === 1 ? createSql : loginQuerySql
-   return mysqlConnection({
-            res,
-            querySql,
+    mysqlConnection({res,querySql}).then(res_=>{
+        res.send(success(true,{data:null}))
+    }).catch(err => {
+        res.send(success(false,{data:null}))
     })
-}
+})
 //=> 查询部署记录
 route.get('/record', (req, res) => {
     const {name,remark_name,item_key,page_num,page_size} = req.query;
