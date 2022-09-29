914ccf4a27944d58b05fd3fc79f1ea38b88b3ac7
wb_jian <wb_jian@meituan.com>
2022-09-29 15:30:51 +0800

    22

--- routes/api/img/index.js
+++ routes/api/img/index.js
@@ -59,7 +59,7 @@ route.post('/voice', (req, res)=>{
             // fs.renameSync(inputFile.path, newPath);
             res.send(success(true,{
                 data:{
-                    path:`/www/file/voice/${uploadedPath}`.replace('/www/file/','http://114.215.183.5:88/')
+                    path:`'http://114.215.183.5:88/voice/${uploadedPath}`
                 }
             }));
           } catch (err) {
