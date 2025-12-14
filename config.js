aa73f4b72b331c9a89d3fa0259efc2c050cce842
yanmeina <yanmeina.com>
2025-12-14 17:43:09 +0800

    增加80端口

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
 
 	//=>CROS跨域相关信息
 	CROS: {
-		ALLOW_ORIGIN: 'http://localhost:3001,http://47.94.11.121:9002,http://119.91.137.119,http://47.94.11.121:4004,http://47.94.11.121:3333,http://47.94.11.121:4000,http://47.94.11.121:9000,http://jianymn.top',
+		ALLOW_ORIGIN: 'http://localhost:3001,,http://47.94.11.121:9002,http://119.91.137.119,http://47.94.11.121:4004,http://47.94.11.121:3333,http://47.94.11.121:4000,http://47.94.11.121:9000,http://47.94.11.121:80,http://jianymn.top',
 		ALLOW_METHODS: 'PUT,POST,GET,DELETE,OPTIONS,HEAD',
 		HEADERS: 'Content-Type,Content-Length,Authorization, Accept,X-Requested-With',
 		CREDENTIALS: true
